e91d8b9bd491be6314ae2f3b5f0769a30a87b1b1
Song M.Q <asongmingqing@qq.com>
2022-06-04 11:49:53 +0800

    材质、光照强度

--- Materials/color.frag
+++ Materials/color.frag
@@ -2,26 +2,41 @@
 out vec4 FragColor;
 in vec3 Normal;
 in vec3 FragPos;
-uniform vec3 objectColor;
-uniform vec3 lightColor;
-uniform vec3 lightPos;
+
 uniform vec3 viewPos;
 
+struct Light {
+    vec3 position;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+uniform Light light;
+
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+}; 
+
+uniform Material material;
+
 void main(){
-    float ambientStrength = .1f;
-    vec3 ambient = ambientStrength * lightColor;
+    vec3 ambient = light.ambient  * material.ambient;
 
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
+    vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.f);
-    vec3 diffuse = diff * lightColor;
+    vec3 diffuse = light.diffuse * (diff * material.diffuse);
 
-    float specularStrength = 0.5;
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 4);
-    vec3 specular = specularStrength * spec * lightColor;
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = light.specular * (spec * material.specular);
 
-    vec3 resultColor = (ambient + diffuse + specular) * objectColor;
+    vec3 resultColor = ambient + diffuse + specular;
     FragColor = vec4(resultColor, 1.f);
 }
--- Materials/main_materials.cpp
+++ Materials/main_materials.cpp
@@ -67,9 +67,14 @@ int main()
 
     Shader lightingShader("color.vert", "color.frag");
     lightingShader.use();
-    lightingShader.setVec3("objectColor", 1.f, 0.5f, 0.31f);
-    lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
-    lightingShader.setVec3("lightPos", lightPos.x, lightPos.y, lightPos.z);
+    lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
+    lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f); // 将光照调暗了一些以搭配场景
+    lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+    lightingShader.setVec3("light.position", lightPos.x, lightPos.y, lightPos.z);
+    lightingShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
+    lightingShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
+    lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
+    lightingShader.setFloat("material.shininess", 32.0f);
     //lightingShader.setVec3("viewPos", camera.Position);
     //lightingShader.setVec3("viewPos", camera.Position.x, camera.Position.y, camera.Position.z);
 
@@ -100,6 +105,17 @@ int main()
         lightingShader.setMat4("view", view);
         lightingShader.setMat4("projection", projection);
         lightingShader.setVec3("viewPos", camera.Position);
+
+        glm::vec3 lightColor;
+        lightColor.x = sin(glfwGetTime() * 2.0f);
+        lightColor.y = sin(glfwGetTime() * 0.7f);
+        lightColor.z = sin(glfwGetTime() * 1.3f);
+
+        glm::vec3 diffuseColor = lightColor * glm::vec3(0.5f); // 降低影响
+        glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f); // 很低的影响
+
+        lightingShader.setVec3("light.ambient", ambientColor);
+        lightingShader.setVec3("light.diffuse", diffuseColor);
 
 
         glBindVertexArray(lightVAO);
