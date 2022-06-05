95b5afcd142cfda653e953474736579d2215eb87
Song M.Q <asongmingqing@qq.com>
2022-06-05 13:31:35 +0800

    修改为点光源投射

--- LightCasters/main_lightcasters.cpp
+++ LightCasters/main_lightcasters.cpp
@@ -107,8 +107,8 @@ int main()
     }
     if (Mode == SPOT)
     {
-        lightingShader.setVec3("light.position", camera.Position);
-        lightingShader.setVec3("light.direction", camera.Front);
+        lightingShader.setVec3("light.position", lightPos);
+        lightingShader.setVec3("light.direction", cubePositions[0] - lightPos);
         lightingShader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
         lightingShader.setFloat("light.outerCutOff", glm::cos(glm::radians(17.5f)));
 
@@ -157,8 +157,8 @@ int main()
 
         if (Mode == SPOT)
         {
-            lightingShader.setVec3("light.position", camera.Position);
-            lightingShader.setVec3("light.direction", camera.Front);
+            lightingShader.setVec3("light.position", lightPos);
+            lightingShader.setVec3("light.direction", cubePositions[0] - lightPos);
             lightingShader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
             lightingShader.setFloat("light.constant", 1.0f);
             lightingShader.setFloat("light.linear", 0.09f);
