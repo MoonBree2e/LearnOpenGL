accab4eccedddb80a379d7d2aedd2de1ef916a51
Song M.Q <asongmingqing@qq.com>
2022-07-21 21:55:56 +0800

    add buffer class

--- Particles/particle_renderer.h
+++ Particles/particle_renderer.h
@@ -2,7 +2,11 @@
 #ifndef _RENDERER_H
 #define _RENDERER_H
 #include <random>
-#include<vector>
+#include <vector>
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+
 
 
 #endif
---
+++ Utils/buffer.h
@@ -0,0 +1,65 @@
+#pragma once
+#include <vector>
+
+#include <glad/glad.h>
+#include <spdlog/spdlog.h>
+
+namespace glcs {
+    class Buffer
+    {
+    private:
+        GLuint BufferID;
+        uint32_t BufferSize;
+    public:
+        Buffer() : BufferID{ 0 }, BufferSize{ 0 }
+        {
+            glCreateBuffers(1, &BufferID);
+
+            spdlog::info("[Buffer] created buffer {:x}", BufferID);
+        }
+        Buffer(const Buffer& buffer) = delete;
+        Buffer(Buffer&& vOther) : BufferID(vOther.BufferID), BufferSize(vOther.BufferSize)
+        {
+            vOther.BufferID = 0;
+        }
+        Buffer& operator=(const Buffer& buffer) = delete;
+        Buffer& operator=(Buffer&& other)
+        {
+            if (this != &other) {
+                release();
+
+                BufferID = other.BufferID;
+                BufferSize = other.BufferSize;
+
+                other.BufferID = 0;
+            }
+            return *this;
+        }
+        ~Buffer() { release(); }
+        
+        void release()
+        {
+            if (BufferID)
+            {
+                spdlog::info("[Buffer] release buffer {:x}", BufferID);
+
+                glDeleteBuffers(1, &BufferID);
+                this->BufferID = 0;
+            }
+        }
+        GLuint getName() const { return BufferID; }
+        uint32_t getLength() const { return BufferSize; }
+
+        template <typename T>
+        void setData(const std::vector<T>& data, GLenum usage)
+        {
+            glNamedBufferData(this->BufferID, sizeof(T) * data.size(), data.data(), usage);
+            this->BufferSize = data.size();
+        }
+
+        void bindToShaderStorageBuffer(GLuint binding_point_index) const
+        {
+            glBindBufferBase(GL_SHADER_STORAGE_BUFFER, binding_point_index, BufferID);
+        }
+    };
+};
